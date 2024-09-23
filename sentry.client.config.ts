94a1c6d92f1e1cfd1ca217b3620375f8b8530b31
Mamad Jadali <jadalimamad@gmail.com>
2024-09-23 16:22:07 +0330

    init

--- sentry.client.config.ts
+++ sentry.client.config.ts
@@ -5,7 +5,7 @@
 import * as Sentry from "@sentry/nextjs";
 
 Sentry.init({
-  dsn: "https://3d627de24f5d06a1fc39000a06ca9a94@o4506813739368448.ingest.us.sentry.io/4507458386526208",
+  dsn: "https://cb55735293346b0c3d99c326fc78a19c@o4507171582771200.ingest.de.sentry.io/4508002101952592",
 
   // Adjust this value in production, or use tracesSampler for greater control
   tracesSampleRate: 1,
