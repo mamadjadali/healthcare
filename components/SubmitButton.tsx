ecea44429af65f323a3eaed91325b2728478e615
Mamad Jadali <jadalimamad@gmail.com>
2025-05-28 15:40:28 +0330

    btn

--- components/SubmitButton.tsx
+++ components/SubmitButton.tsx
@@ -13,7 +13,7 @@ const SubmitButton = ({ isLoading, className, children }: ButtonProps) => {
     <Button
       type="submit"
       disabled={isLoading}
-      className={className ?? "shad-primary-btn w-full"}
+      className={className ?? "shad-primary-btn w-full rounded-xl"}
     >
       {isLoading ? (
         <div className="flex items-center gap-4">
