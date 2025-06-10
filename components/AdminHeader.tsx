27997593f68e597ee779660aea112a6342691024
Mamad Jadali <jadalimamad@gmail.com>
2025-06-10 11:19:47 +0330

    badge, DR

--- components/AdminHeader.tsx
+++ components/AdminHeader.tsx
@@ -5,9 +5,10 @@ import Link from "next/link";
 import { useRouter } from "next/navigation";
 import { Button } from "./ui/button";
 import { setLocalStorage } from "@/lib/utils";
-import { LogOut } from "lucide-react";
+import { BadgeCheckIcon, LogOut } from "lucide-react";
 import { SettingsSheet } from "@/components/SettingsSheet";
 import { TimeSettingsSheet } from "@/components/TimeSettingsSheet";
+import { Badge } from "./ui/badge";
 
 export const AdminHeader = () => {
   const router = useRouter();
@@ -30,7 +31,22 @@ export const AdminHeader = () => {
             className="h-8 w-fit"
           />
         </Link>
-
+        <button className="hidden md:flex relative h-6 overflow-hidden rounded-lg p-[1px] focus:outline-none">
+        <span className="absolute inset-[-1000%] animate-[spin_2s_linear_infinite] bg-[conic-gradient(from_90deg_at_50%_50%,#E2CBFF_0%,#393BB2_50%,#E2CBFF_100%)]" />
+        <span className="inline-flex h-full w-full cursor-pointer items-center justify-center rounded-lg bg-transparent px-3 py-1 text-sm font-medium text-white backdrop-blur-3xl">
+          Dr.Maryam Ranjbar
+        </span>
+      </button>
+        <Badge
+          variant="secondary"
+          className="hidden md:flex bg-[#155dfc] text-white dark:bg-blue-600"
+        >
+          <span className="relative flex size-3 mr-2">
+            <span className="absolute inline-flex h-full w-full animate-ping rounded-full bg-white opacity-75"></span>
+            <span className="relative inline-flex size-3 rounded-full bg-slate-300"></span>
+            </span>
+           v0.1.0 – Mercury
+        </Badge>
         <div className="flex items-center gap-4">
           <SettingsSheet />
           <TimeSettingsSheet />
